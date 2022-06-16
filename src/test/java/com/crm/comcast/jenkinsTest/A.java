c5b5f6c508fdb654b41b4d3b5407aa81a6093cca
Asus <Asus@ADMIN>
2022-06-16 16:39:08 +0530

    firsr commit

---
+++ src/test/java/com/crm/comcast/jenkinsTest/A.java
@@ -0,0 +1,10 @@
+package com.crm.comcast.jenkinsTest;
+
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ src/test/java/com/crm/comcast/jenkinsTest/B.java
@@ -0,0 +1,10 @@
+package com.crm.comcast.jenkinsTest;
+
+public class B {
+
+	public static void main(String[] args) {
+		System.out.println("World");
+
+	}
+
+}
